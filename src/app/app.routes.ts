bd8c039fa3ec9b618bfb5e80554e079d29d01973
jimmytheneutrino <aleksei.lissitsin@gmail.com>
2017-03-28 11:11:09 +0300

    login and authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import {LoginComponent} from "./auth/login.component";
+import {AuthGuard} from "./auth/auth-guard.service";
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
@@ -13,6 +15,10 @@ export const ROUTES: Routes = [
   { path: 'search',  component: SearchComponent },
   { path: 'about', component: AboutComponent },
   { path: 'detail', component: ObjectDetailComponent },
-  { path: 'payment',    component: PaymentComponent },
+  {
+    path: 'payment',
+    canActivate: [AuthGuard],
+    component: PaymentComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**',    component: NoContentComponent },
 ];
