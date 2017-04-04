3b737f65738b3cc84028421e9d50d1687d94dea3
mrcerf <mrcerf@gmail.com>
2017-04-04 10:25:37 +0300

    inputs

--- src/app/common/ui/emta-form-row.component.ts
+++ src/app/common/ui/emta-form-row.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ContentChild, Input} from '@angular/core';
+import { EmtaInputComponent } from './emta-input.component';
 
 @Component({
   selector: 'emta-form-row',
   templateUrl: './emta-form-row.component.html',
   inputs: [
-    'name',
     'label',
     'comment'
   ]
 })
 export class EmtaFormRowComponent implements OnInit {
+  @Input() private name: string;
+  @ContentChild(EmtaInputComponent) private child;
 
   public ngOnInit() {
-    // nothing on init
+    //
+  }
+
+  public  ngAfterContentInit() {
+    if (!this.name && !!this.child && !!this.child.name) {
+      this.name = this.child.name;
+    }
   }
 
 }
