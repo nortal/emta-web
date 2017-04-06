b6a676e25b04efe5a2b59bdeb068dea74f391c99
mrcerf <mrcerf@gmail.com>
2017-04-06 10:06:58 +0300

    emta-header host class

--- src/app/common/ui/list/emta-th.component.ts
+++ src/app/common/ui/list/emta-th.component.ts
@@ -3,11 +3,16 @@ import { SortInstance } from './sort-instance';
 
 @Component({
   selector: '[sortable]',
-  templateUrl: './emta-th.component.html'
+  templateUrl: './emta-th.component.html',
+  host: {'[class]': 'getHostClass()'}
 })
 export class EmtaThComponent implements OnInit {
   @Input() public field: any;
   @Input() public sort: SortInstance;
+
+  public getHostClass() {
+    return this.sort && this.field === this.sort.field ? this.sort.direction === 'asc' ? 'sorted-ascending' : 'sorted-descending' : '';
+  }
 
   public ngOnInit() {
     if (!this.sort) {
