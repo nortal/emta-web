f84a7b7ce569e963a0e464b2a459303f7b65a16b
urmet <urmet.tamberg@nortal.com>
2017-04-25 12:18:17 +0300

    datepicker uue kujundusega, pooleli

--- src/app/common/ui/emta-datepicker.component.ts
+++ src/app/common/ui/emta-datepicker.component.ts
@@ -1,81 +1,133 @@
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Component, Input, forwardRef } from '@angular/core';
-import { TranslateService } from "ng2-translate";
-import { IMyOptions } from "mydatepicker/index";
+import { TranslateService, LangChangeEvent } from "ng2-translate";
+import { IMyOptions, IMyDayLabels } from "mydatepicker/index";
+import {IMyMonthLabels} from "mydatepicker/index";
 
 @Component({
-  selector: 'emta-datepicker',
-  templateUrl: './emta-datepicker.component.html',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => EmtaDatepickerComponent),
-      multi: true
-    }
-  ]
+    selector: 'emta-datepicker',
+    templateUrl: './emta-datepicker.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => EmtaDatepickerComponent),
+            multi: true
+        }
+    ]
 })
 export class EmtaDatepickerComponent implements ControlValueAccessor {
-  @Input() public minDate: Date;
-  @Input() public maxDate: Date;
-  public value: Date;
-  public onChange: (event: any) => any = (x) => x;
+    @Input() public minDate:Date;
+    @Input() public maxDate:Date;
+    public value:Date;
+    public onChange:(event:any) => any = (x) => x;
 
-  public constructor(public translate: TranslateService) {
-  }
-
-  private myDatePickerOptions: IMyOptions = {
-    todayBtnTxt: 'Täna',
-    dayLabels: {su: 'P', mo: 'E', tu: 'T', we: 'K', th: 'N', fr: 'R', sa: 'L'},
-    monthLabels: { 1: 'Jaanuar', 2: 'Veebruar', 3: 'Märts', 4: 'Aprill', 5: 'Mai', 6: 'Juuni', 7: 'Juuli', 8: 'August', 9: 'September', 10: 'Oktoober', 11: 'November', 12: 'Detsember' },
-    showTodayBtn: false,
-    firstDayOfWeek: 'mo',
-    height: '34px',
-    width: '260px',
-    dateFormat: 'dd.mm.yyyy',
-    editableMonthAndYear: false,
-    markWeekends: {marked: true}
-//    inline: true
-  };
-
-  private dateValueObject: Object = { date: { year: 2018, month: 10, day: 9 } };
-
-  public onSelectionDone(event: any, pop: any) {
-  /*  setTimeout(() => {
-      pop.hide();
-    }, 10);
-    */
-    this.onChange(event);
-  }
-
-  public dateText() {
-    return this.value ? this.value.toLocaleString(
-      this.translate.currentLang, {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }) : '';
-  }
-
-  /**
-   * Write a new value to the element.
-   */
-  public writeValue(obj: any) {
-    if (obj && obj instanceof Date) {
-      this.value = obj;
+    private localizedDayLabels:{et: IMyDayLabels, en: IMyDayLabels, ru: IMyDayLabels} = {
+        et: {su: 'P', mo: 'E', tu: 'T', we: 'K', th: 'N', fr: 'R', sa: 'L'},
+        en: {su: 'Su', mo: 'Mo', tu: 'Tu', we: 'We', th: 'Th', fr: 'Fr', sa: 'Sa'},
+        ru: {su: 'ruSu', mo: 'ruMo', tu: 'ruTu', we: 'We', th: 'Th', fr: 'Fr', sa: 'Sa'}
     }
-  }
 
-  /**
-   * Set the function to be called when the control receives a change event.
-   */
-  public registerOnChange(fn: any) {
-    this.onChange = fn;
-  }
+    private localizedMonthLabels:{et: IMyMonthLabels, en: IMyMonthLabels, ru: IMyMonthLabels} = {
+        et: {
+            1: 'Jaanuar',
+            2: 'Veebruar',
+            3: 'Märts',
+            4: 'Aprill',
+            5: 'Mai',
+            6: 'Juuni',
+            7: 'Juuli',
+            8: 'August',
+            9: 'September',
+            10: 'Oktoober',
+            11: 'November',
+            12: 'Detsember'
+        },
+        en: {
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'Octoober',
+            11: 'November',
+            12: 'December'
+        },
+        ru: {
+            1: 'ruJaanuar',
+            2: 'Veebruar',
+            3: 'Märts',
+            4: 'ruAprill',
+            5: 'Mai',
+            6: 'Juuni',
+            7: 'Juuli',
+            8: 'August',
+            9: 'September',
+            10: 'Oktoober',
+            11: 'November',
+            12: 'Detsember'
+        }
+    }
 
-  /**
-   * Set the function to be called when the control receives a touch event.
-   */
-  public registerOnTouched(fn: any) {
-    console.log('registerOnTouched');
-  }
+    public constructor(public translate:TranslateService) {
+        translate.onLangChange.subscribe((params:LangChangeEvent) => {
+            var newOptions = Object.assign({}, this.myDatePickerOptions);
+            newOptions.dayLabels = this.localizedDayLabels[params.lang];
+            newOptions.monthLabels = this.localizedMonthLabels[params.lang];
+            this.myDatePickerOptions = newOptions;
+        });
+    }
+
+    private myDatePickerOptions:IMyOptions = {
+        todayBtnTxt: 'Täna',
+        showTodayBtn: false,
+        firstDayOfWeek: 'mo',
+        height: '34px',
+        width: '260px',
+        dateFormat: 'dd.mm.yyyy',
+        editableMonthAndYear: false,
+        markWeekends: {marked: true, color: 'red'}
+    };
+
+
+    private dateValueObject:Object; // = { date: { year: 2018, month: 10, day: 9 } };
+
+    public onSelectionDone(event:any, pop:any) {
+        this.onChange(event);
+    }
+
+    public dateText() {
+        return this.value ? this.value.toLocaleString(
+            this.translate.currentLang, {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            }) : '';
+    }
+
+    /**
+     * Write a new value to the element.
+     */
+    public writeValue(obj:any) {
+        if (obj && obj instanceof Date) {
+            this.value = obj;
+        }
+    }
+
+    /**
+     * Set the function to be called when the control receives a change event.
+     */
+    public registerOnChange(fn:any) {
+        this.onChange = fn;
+    }
+
+    /**
+     * Set the function to be called when the control receives a touch event.
+     */
+    public registerOnTouched(fn:any) {
+        console.log('registerOnTouched');
+    }
 }
